987d36b8ed39d233e9c550725f848cb7dd426cd4
hanqing <1126643825@qq.com>
2019-07-17 22:21:49 +0800

    lodash

--- miao/lodash/hanqing0328-lodash.js
+++ miao/lodash/hanqing0328-lodash.js
@@ -1,5 +1,50 @@
-var hanqing0328 = {
-  compact : function(arry){
-    return arry.filter(it => it)
+var hanqing0328 = function () {
+  
+/**
+ * compact
+ * Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+ * @param {*} arry 
+ * @returns (Array): Returns the new array of filtered values.
+ * Example :
+ * input:compact([0, 1, false, 2, '', 3])
+ * out:[1, 2, 3]
+ */
+  function compact(ary) {
+    return ary.filter(it => it)
   }
-}
+
+
+
+/**
+ * every
+ * Checks if predicate returns truthy for all elements of collection. Iteration is stopped once predicate returns falsey.
+ * @param collection (Array|Object)
+ * @param [predicate=_.identity] (Function)
+ * @returns (boolean): Returns true if all elements pass the predicate check, else false.
+ * @Example
+ * input:every([true, 1, null, 'yes'], Boolean)
+ * output:false
+ */
+  function every(ary, predicate) {
+    for(var i = 0 ; i < ary.length ; i++) {
+      if(!predicate(ary[i])) {
+        return false
+      }
+    }
+    return true
+  }
+
+
+
+
+
+
+
+
+  return {
+    compact,
+    every,
+
+
+  }
+} ()
